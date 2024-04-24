f9b14c8cfb4df40a284b9ea256835f54a2224103
Rajdeep <1rajofficial0@gmail.com>
2024-04-24 12:52:23 +0530

    programmin stake part-2

---
+++ sorting/bubblesort.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int n;
+void bubblesort();  
+
+//function to print the array
+void printArray(int arr[]){
+    for(int i=0;i<n;i++)
+        printf("%d ",arr[i]);
+}
+
+
+int main(){
+    
+    //time variable
+    time_t t;
+
+    //taking the size of the array from the user
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    int ar[n];
+
+    //randomized numbers for the array,taking the size from the user and filling it up with random numbers
+    srand ( (unsigned) time (&t));
+    for(int i=0;i<n;i++){
+        ar[i]=(rand()%100);
+    }
+
+    //Before sorting , the array will print the randomized number
+    printf("Before sorting: ");
+    printArray(ar);
+
+
+    //After sorting, the array will print the sorted array in ascending order
+    bubblesort(ar);
+    printf("\nAfter sorting: ");
+    printArray(ar);
+
+    return 0;
+}
+
+//function to sort the array using bubble sort method
+void bubblesort(int arr[]){
+    for(int i=0;i<(n-1);i++)
+    {
+        for(int j=0;j<(n-1-i);j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+
+}
